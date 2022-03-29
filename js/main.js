a8d2acd4c395a357d2a678f0ae6a860bfef113f5
huy.pham.0508 <phammanhhuy1107@gmail.com>
2022-03-29 23:11:16 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -15,6 +15,14 @@ $(".customer-content").slick({
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow:
+          "<button type='button' class='slick-prev pull-left'><ion-icon name='arrow-back-outline'></ion-icon></button>",
+        nextArrow:
+          "<button type='button' class='slick-next pull-right'><ion-icon name='arrow-forward-outline'></ion-icon></button>",
       },
     },
   ],
